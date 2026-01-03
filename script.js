7b112b70078faab16904026b64a34d9ce499fc23
Dertme314 <werewould4@gmail.com>
2026-01-03 16:09:12 -0500

    words

--- script.js
+++ script.js
@@ -289,7 +289,7 @@ function appendLoader() {
     div.id = 'temp-loader';
     div.className = 'message bot-message';
     // UI CHANGE: Spinning icon instead of text
-    div.innerHTML = `<span class="material-symbols-outlined" style="animation:spin 0.01s infinite; font-size:24px;">sync</span>`;
+    div.innerHTML = `<span class="material-symbols-outlined" style="animation:spin 0.01s infinite; font-size:24px;">sync</span> Thinking...`;
     
     // Add the keyframe animation if not in CSS
     if (!document.getElementById('loader-style')) {
